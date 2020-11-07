e01061b16bb70871f3681e541c62abcd58c005d7
shubham <shubham@zipcodes-MBP-5.fios-router.home>
2020-11-07 10:36:08 -0500

    properties of cigar added

--- src/main/java/models/Cigar.java
+++ src/main/java/models/Cigar.java
@@ -1,4 +1,12 @@
 package models;
 
 public class Cigar {
+
+    private int id;
+    private String name;
+    private String brand;
+    private String shape;
+    private float size;
+    private int quantity;
+    private float price;
 }
